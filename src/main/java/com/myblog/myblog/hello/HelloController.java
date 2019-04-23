6c853633f605cb58aa139e67767cdae9e9ffd007
wang.junxin111 <wang.junxin111@iwhalecloud.com>
2019-04-23 14:01:49 +0800

    上传项目

---
+++ src/main/java/com/myblog/myblog/hello/HelloController.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/hello" ,method = RequestMethod.GET)
+    public String helloWorld(){
+        return "欢迎!!";
+    }
+}
